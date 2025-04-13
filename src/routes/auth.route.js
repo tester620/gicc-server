197e0d5ef64ceafd9433196ad79f3f700ef69830
tester620 <dheerajverma82664442@gmail.com>
2025-04-13 16:44:59 +0530

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { signup } from "../controllers/auth.controller.js";
+import { login } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 
-router.post("/signup",signup)
+router.post("/login",login)
 export default router;
