c96207f44627b091d06a828843649b87663551a3
tester620 <dheerajverma82664442@gmail.com>
2025-04-16 14:55:26 +0530

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
-import { config } from "dotenv";
+import dotenv  from "dotenv";
 
-config();
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
 });
 export default cloudinary;
